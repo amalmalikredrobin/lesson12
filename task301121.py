5db9452cf710aa915b122a6059d8ca861a3c6f85
Amalmalik Abuzarov <amalmalikredrobin@gmail.com>
2021-11-30 20:00:52 +0500

    Last commit

---
+++ task301121.py
@@ -0,0 +1,41 @@
+# def calcPow(base, exp):
+#     return base ** exp
+
+# print(calcPow(3, 4))
+
+# def calcPow(base, exp):
+#     return base ** exp
+
+# print(calcPow(3, 4))
+
+# def reverseList(List):
+#     return List[::-1]
+
+# print(reverseList([1, 2, 3]))
+
+# def calcSum(a, b):
+#     return a + b
+# print(calcSum(2,3))
+
+# def calcSum(a, b, *c):
+#     acc = 0
+#     for num in c:
+#         acc += num
+    
+#     return a + b + acc
+
+# print(calcSum(2,3))
+
+
+# def sayHello(name, surname):
+#     print(f'Hi, {name} {surname}')
+
+# sayHello(surname='Abuzarov', name='Amalmalik')
+# sayHello('Amalmalik', 'Abuzarov')
+
+def min(a, b):
+    if a < b: a
+    else: return b
+
+print(min(10, 20))
+print(min(10, 5))
