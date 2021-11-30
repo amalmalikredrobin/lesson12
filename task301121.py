b3d86d9a95d7b5246920fae07eaf7e91e79ca5f8
Amalmalik Abuzarov <amalmalikredrobin@gmail.com>
2021-11-30 20:01:41 +0500

    Finish

--- task301121.py
+++ task301121.py
@@ -33,9 +33,27 @@
 # sayHello(surname='Abuzarov', name='Amalmalik')
 # sayHello('Amalmalik', 'Abuzarov')
 
-def min(a, b):
-    if a < b: a
-    else: return b
+# def min(a, b):
+#     if a < b: a
+#     elif a == b: return 'Equal'
+#     else: return b+
 
-print(min(10, 20))
-print(min(10, 5))
+# print(min(10, 20))
+# print(min(10, 5))
+# print(min(10, 10))
+
+
+# print(eval('1/2'))
+
+
+
+# def calculator(a, b, action):
+#     if action == 'add': return eval(f'{a}+{b}') 
+#     if action == 'subtract': return eval(f'{a}-{b}') 
+#     if action == 'multiply': return eval(f'{a}*{b}') 
+#     if action == 'divide': return eval(f'{a}/{b}') 
+
+# print(calculator('1', '3', 'add'))
+# print(calculator('10', '6', 'subtract'))
+# print(calculator('10', '6', 'multiply'))
+# print(calculator('10', '6', 'divide'))
